64bd6c01f446e76b8f468c54428a4fef09321188
chrgeorgiou <chr.p.georgiou@gmail.com>
2020-04-20 19:03:16 +0200

    Input power spectra from CCL. Growth rate and factor from CCL.

--- tests/test_ccl.py
+++ tests/test_ccl.py
@@ -1,9 +1,3 @@
-# semi-pseudo-code of what CCL theory might look like
-
-# For Cobaya docs see
-# https://cobaya.readthedocs.io/en/devel/theory.html
-# https://cobaya.readthedocs.io/en/devel/theories_and_dependencies.html
-
 import numpy as np
 from cobaya.theory import Theory
 
@@ -17,12 +11,12 @@ class CCL(Theory):
         self._var_pairs = set()
 
     def get_requirements(self):
+        # These are required to construct a CCL cosmology object.
+        # 'As' could be substituted by sigma8.
         return {'omch2', 'ombh2', 'H0', 'ns', 'As'}
-        #return
 
     def needs(self, **requirements):
         # requirements is dictionary of things requested by likelihoods
-        # check all requirements, and find what CCL needs to compute them
         # Note this may be called more than once
         self._var_pairs = {('delta_tot', 'delta_tot')}  # fix for now
 
@@ -31,6 +25,7 @@ class CCL(Theory):
             self._kmax = max(10, self._kmax)
             self._zmax = max(5, self._zmax)
         """
+            # This is for when more power spectra can be read into CCL.
             if cl.get('WeakLensingTracer'):
                 self._var_pairs.add(('Weyl', 'Weyl'))
             if cl.get('NumberCountsTracer'):
@@ -38,35 +33,21 @@ class CCL(Theory):
         # similarly for cross-spectra etc
         """
 
-        # make a dictionary of the things CCL needs from CAMB/CLASS
-        # see https://cobaya.readthedocs.io/en/devel/cosmo_theories_likes.html
+        # Dictionary of the things CCL needs from CAMB/CLASS
         needs = {}
         # sampling_step = self._zmax * 0.1
         # self._z_sampling = np.arange(0, self._zmax, sampling_step)
         self._z_sampling = np.linspace(0, self._zmax, 100) # Fixed at 100 steps to z max for now
         if self._kmax:
             needs['Pk_grid'] = {'vars_pairs': [('delta_tot', 'delta_tot')],
-                                'nonlinear': False,  # allows both linear and non-linear
+                                'nonlinear': (True, False),
                                 'z': self._z_sampling,
                                 'k_max': self._kmax
                                 }
-        #            needs['baryon_fudge1'] = None  # nuisance parameter needed
+        #    needs['baryon_fudge1'] = None  # nuisance parameter needed
         if self._zmax:
-            pass
             needs['Hubble'] = {'z': self._z_sampling}
             needs['comoving_radial_distance'] = {'z': self._z_sampling}
-            """
-            needs['growth'] = {'z': self._z_sampling} # FIXME: How do I retrieve these?
-            needs['fgrowth'] = {'z': self._z_sampling}
-            """
-
-        # if any cosmological parameters needed can add them to requirements
-        # Which cosmo parameters are necessary, (e.g. A_s, H0 to go from E(z) to H(z))
-        needs['H0'] = None
-        needs['As'] = None
-        needs['omch2'] = None
-        needs['ombh2'] = None
-        needs['ns'] = None
 
         return needs
 
@@ -79,61 +60,56 @@ class CCL(Theory):
         # retrieved as needed by get_angular_cl() and other methods
         # get our requirements from self.provider
         if self._zmax:
-            pass
-            # TODO: Here the CCL functions that return chi(z), E(z)? Note: CCL accepts scale factor as variable, change z to a.
-            # distance = self.provider.get_comoving_radial_distance(self._z_sampling)
+            # Read in the distance and Hubble parameter arrays:
+            distance = self.provider.get_comoving_radial_distance(self._z_sampling)
+            hubble_z = self.provider.get_Hubble(self._z_sampling)
+            E_of_z = hubble_z/self.provider.get_param('H0')
+            # Array z is sorted in ascending order. CCL requires an ascending scale factor as input
+            # Flip the arrays to make them a function of the increasing scale factor.
+            # If redshift sampling is changed, check that it is monotonically increasing
+            distance = np.flip(distance)
+            E_of_z = np.flip(E_of_z)
 
         if self._kmax:
-            # Create a CCL cosmology object
-            h = self.provider.get_param('H0') / 100.
-            Omega_c = self.provider.get_param('omch2') / h ** 2.
-            Omega_b = self.provider.get_param('ombh2') / h ** 2.
-            #baryon_fudge1 = params_values_dict['baryon_fudge1']
             state['angular_cl'] = {}
             for pair in self._var_pairs:
                 # Get the matter power spectrum:
-                k, z, Pk = self.provider.get_Pk_grid(var_pair=pair,
-                                                          nonlinear=False)
+                k, z, Pk_lin = self.provider.get_Pk_grid(var_pair=pair, nonlinear=False)
+                k, z, Pk_nonlin = self.provider.get_Pk_grid(var_pair=pair, nonlinear=True)
                 # Array z is sorted in ascending order. CCL requires an ascending scale factor as input
-                # np.flip(arr, axis=0) flips the rows of arr, thus making PK_Weyl with z in descending order.
-                Pk = np.flip(Pk, axis=0)
+                # np.flip(arr, axis=0) flips the rows of arr, thus making Pk with z in descending order.
+                Pk_lin = np.flip(Pk_lin, axis=0)
+                Pk_nonlin = np.flip(Pk_nonlin, axis=0)
                 a = np.sort(1. / (1 + z))
-                # Read in the distance and Hubble parameter arrays:
-                distance = self.provider.get_comoving_radial_distance(self._z_sampling)
-                hubble_z = self.provider.get_Hubble(self._z_sampling)
-                E_of_z = hubble_z/self.provider.get_param('H0')
-                #import pdb; pdb.set_trace()
-                # FIXME:
-                # growth = self.provider.get_???
-                # fgrowth = self.provider.get_???
-                # FIXME: Check that these are okay with redshift->scale factor.
-                # .. use them and store calculated result
-                # TODO: Here create a cosmo object with paramteres read from input.
-                # cosmo = CCL(distance=distance, k=k, z=z, PKWell=PK_Weyl ...)
-                transfer_function = 'pklin_from_input'  # this to read linear power spectrum.
-                #matter_pk = 'pkln_from_input'  # This to read non-linear power spectrum.
-                matter_pk = 'linear'
-                # These are the bare essentials to call the Cosmology object.
+
+                # Create a CCL cosmology object
                 import pyccl as ccl
+                transfer_function = 'pklin_from_input'  # this to read linear power spectrum.
+                matter_pk = 'pknl_from_input'  # This to read non-linear power spectrum.
+                h = self.provider.get_param('H0') / 100.
+                Omega_c = self.provider.get_param('omch2') / h ** 2.
+                Omega_b = self.provider.get_param('ombh2') / h ** 2.
+                #baryon_fudge1 = params_values_dict['baryon_fudge1']
+                # Currently, CCL requires the linear matter perturbations growth factor and rate.
+                # Compute these from CCL:
+                cosmo = ccl.Cosmology(Omega_c=Omega_c, Omega_b=Omega_b, h=h,
+                                      n_s=self.provider.get_param('ns'),
+                                      A_s=self.provider.get_param('As'))
+                growth = ccl.background.growth_factor(cosmo, a)
+                fgrowth = ccl.background.growth_rate(cosmo, a)
+                # In order to use CCL with input arrays, the cosmology object needs to be reset.
                 cosmo = ccl.Cosmology(Omega_c=Omega_c, Omega_b=Omega_b, h=h,
                                       n_s=self.provider.get_param('ns'),
                                       A_s=self.provider.get_param('As'),
-                                      #transfer_function=transfer_function,
+                                      transfer_function=transfer_function,
                                       matter_power_spectrum=matter_pk)
-                """
                 cosmo._set_background_from_arrays(a_array=a,
                                                   chi_array=distance,
                                                   hoh0_array=E_of_z,
                                                   growth_array=growth,
                                                   fgrowth_array=fgrowth)
-                """
-                # FIXME: if linear power, else non-linear
-                #cosmo._set_linear_power_from_arrays(a, k, Pk)
-                #cosmo._set_nonlin_power_from_arrays(a, k, Pk)
-
-                # TODO: Comment this out once the part above is working
-                P_of_k_a = ccl.pk2d.Pk2D(a_arr=a, lk_arr=np.log(k), pk_arr=Pk,
-                                          is_logp=False)
+                cosmo._set_linear_power_from_arrays(a, k, Pk_lin)
+                cosmo._set_nonlin_power_from_arrays(a, k, Pk_nonlin)
 
                 # FIXME: CCL angular_cl requires 2 tracers. How to build those?
                 z_n = np.linspace(0., 1., 200)
@@ -141,11 +117,8 @@ class CCL(Theory):
                 tracer1 = ccl.WeakLensingTracer(cosmo, dndz=(z_n, n))
                 tracer2 = ccl.WeakLensingTracer(cosmo, dndz=(z_n, n))
                 ell =  np.logspace(np.log10(3),3) # FIXME: At which ell do we compute C_l? Now it is log-spaced from 3 to 1000
-                cls = ccl.cls.angular_cl(cosmo, tracer1, tracer2, ell, p_of_k_a=P_of_k_a)
-                # TODO: Once CCL is reading the PS, comment top and un-comment bottom line.
-                #cls = ccl.cls.angular_cl(cosmo, tracer1, tracer2, ell)
+                cls = ccl.cls.angular_cl(cosmo, tracer1, tracer2, ell)
                 state['angular_cl'][pair] = {'cls': cls, 'ell': ell}
-
 
     def get_angular_cl(self, pair=('delta_tot', 'delta_tot')):
         # function used by likelihoods to get results from CCL
@@ -181,12 +154,12 @@ if __name__ == '__main__':
 
     def cl_likelihood(_theory={'angular_cl': {'_kmax': 2}}):
         results = _theory.get_angular_cl()
-
+        # print(results)
         """
         # Test the C_ls computed using a power spectrum against the C_ls computed directly, in CCL:
         import pyccl as ccl
         cosmo = ccl.Cosmology(Omega_c=omc, Omega_b=omb, h=h,
-                              n_s=ns, A_s=As, m_nu=mnu, transfer_function='boltzmann_camb', matter_power_spectrum='linear')
+                              n_s=ns, A_s=As, m_nu=mnu, transfer_function='boltzmann_camb', matter_power_spectrum='halofit')
         # Define a simple binned galaxy number density curve as a function of redshift
         z_n = np.linspace(0., 1., 200)
         n = np.ones(z_n.shape)
@@ -206,18 +179,9 @@ if __name__ == '__main__':
         plt.ylabel(r'$C_\ell$(cosmo)/$C_\ell$(from Pk)')
         plt.show()
         """
-        # print(results)
         return
 
-    """
-    info = {'likelihood': {'C_l': cl_likelihood},
-            'theory': OrderedDict({
-                'camb': {'stop_at_error': True,
-                         'requires': {'omch2', 'ombh2', 'H0', 'ns', 'As'}},
-                'CCL': {'external': CCL}}),
-            'params': dict(H0={'prior': {'min': 0, 'max': 100}}, **camb_params),
-            'debug': debug, 'stop_at_error': True}
-    """
+
     info = {'likelihood': {'C_l': cl_likelihood},
             'theory': OrderedDict({
                 'camb': {'stop_at_error': True},
